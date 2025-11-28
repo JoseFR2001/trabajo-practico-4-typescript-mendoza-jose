a490fc548d07901da8abe1bbdb7eaf37795f18f9
JoseFR2001 <jose.rafael.mendoza.2001@gmail.com>
2025-11-28 01:54:11 -0300

    Desarrollo del Punto 1: Tipos fundamentales, uniones y el objeto Product

---
+++ src/consigna1.ts
@@ -0,0 +1,27 @@
+//Tipos Fundamentales
+let productName : string = "Acuarius";
+console.log(productName)
+
+let productPrice : number = 3500;
+console.log(productPrice)
+
+//Tipos Union
+let productId : string | number
+
+productId = "10"
+console.log(productId)
+
+productId = 20
+console.log(productId)
+
+//Interfaz Básica
+interface Product {
+    name: string;
+    price: number;
+}
+
+let myProduct: Product = {
+    name: "Cepita",
+    price: 2500
+}
+console.log(myProduct)
